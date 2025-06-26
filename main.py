4209dbc476ff060ef9de5c4be029c181a7d30d3d
tito69-hola <he.benavente@duocuc.cl>
2025-06-26 12:39:16 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import funciones
+
+menu = """----------
+TOTEM AUTOATENCIÓN RESERVA STRIKE
+1.- Reservar zapatillas
+2.- Buscar zapatillas reservadas.
+3.- Cancelar reserva de zapatillas.
+4.- Salir.
+----------
+"""
+while True:
+    print(menu)
+    input("que necesita: ")
+    if menu == "1":
+        pass
+    if menu == "2":
+        pass
+    if menu == "3":
+        pass
+    if menu == "4":
+        print("vuelva luego!")
+    break
+
+
